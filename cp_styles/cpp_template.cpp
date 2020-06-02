db46ed1e3b00763d0396aa7923b8d7cd9d638163
Surya Prakash Sahu <suryathe16@gmail.com>
2020-06-02 12:30:38 +0530

    Create cpp_template.cpp

---
+++ cp_styles/cpp_template.cpp
@@ -0,0 +1,55 @@
+//  author	: thepurpleowl
+
+#include <bits/stdc++.h>
+#define fast	    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define ll         long long
+
+//pair
+#define pii      pair<int,int>
+#define fi      first
+#define sec     second
+
+//vector
+#define vi          vector<int>
+#define vl          vector<long>
+#define vii         vector<pii>
+#define vs          vector<string>
+#define sz(x)       (int)x.size()
+#define pb          push_back
+ 
+//matrice
+#define vvi	   vector<vector<int>>
+#define vvii	    vector<vector<pii>>
+ 
+//map
+#define mi          map<int,int>
+#define mii         map<pii,int>
+ 
+//loop
+#define f(i,a,b)  for(int i=a;i<b;i++)
+#define f0(i,a)  for(int i=0;i<a;i++)
+#define rf(i,a,b)  for(int i=b;i>=a;i--)
+#define rf0(i,a)  for(int i=a;i>=0;i--)
+
+//nec
+#define int         ll
+//#define endl        '\n'
+const int mod  = 1e9;
+ 
+ 
+using namespace std;
+ 
+void solve(){ 
+	
+  return;
+}
+ 
+int main(){
+    fast;
+    int t=1;
+	  cin>>t;
+    f(i,0,t){
+	    solve();
+    }
+    return 0;
+}
