f5edf01c992a4e496366846111adbfdca82c1393
Roshan <techcodno@gmail.com>
2020-10-06 16:08:53 +0530

    Sovled competitive program of java comparator of hackerrank

---
+++ HackerRank/JavaComparator_04.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Checker implements Comparator<Player>{
+    public int compare(Player o1, Player o2){
+        if (o1.score == o2.score){
+            return o1.name.compareTo(o2.name);
+        } else {
+            return o2.score - o1.score; 
+        }
+    }
+}
+
+class Player{
+    String name;
+    int score;
+    
+    Player(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class JavaComparator_04 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+        
+        for(int i = 0; i < n; i++){
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+     
+        Arrays.sort(player, checker);
+        for(int i = 0; i < player.length; i++){
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
+    }
+}
