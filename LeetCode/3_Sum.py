ffdbf300765f9c24f9c1b0dcdb89b7ec4621ad00
Caroline Joseph <carolsjoseph75@gmail.com>
2020-10-24 00:27:19 +0530

    Removed trailing spaces for LeetCode #15 3Sum in Python

--- LeetCode/3_Sum.py
+++ LeetCode/3_Sum.py
@@ -5,7 +5,7 @@ class Solution(object):
             for j in range(i+1, len(nums)-1):
                 for k in range(j+1, len(nums)):
                     if nums[i]+nums[j]+nums[k]==0:
-                        zero = [nums[i],nums[j],nums[k]]    
+                        zero=[nums[i],nums[j],nums[k]]    
                         if sorted(zero) not in li:
                             li.append(sorted(zero))
         return li
