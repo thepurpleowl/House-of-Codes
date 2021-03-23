3a52aba7d355446490987a57b27b52084968148e
Md-Atik-Enam <77339353+Md-Atik-Enam@users.noreply.github.com>
2021-03-23 11:56:03 -0400

    Update rabinKarpStringMatch.java

--- Basics/String_Search/rabinKarpStringMatch.java
+++ Basics/String_Search/rabinKarpStringMatch.java
@@ -66,7 +66,7 @@ public class rabinKarpStringMatch {
 class rabinKarpMain {
     public static void main(String[] args) throws FileNotFoundException {
         String T, P ;
-        Scanner sc = new Scanner("banana") ;
+        Scanner sc = new Scanner("na\nbanana") ;
 
         P = sc.nextLine() ;
         T = sc.nextLine() ;
